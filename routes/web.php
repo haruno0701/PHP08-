7eaf4df1dda1335447c1213e42a42a22f8718ede
haruno naduki <naduki.h.0701@gmail.com>
2024-10-20 23:58:55 +0000

    php12_kadai

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,7 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
-});
-
-Route::controller(NewsController::class)->group(function() {
-    Route::get('admin/news/create', 'add');
-});
-
-use App\Http\Controllers\Admin\AAAController;
-Route::controller(AAAController::class)->group(function() {
-    Route::get('admin/AAA/XXX', 'bbb');
+    Route::get('profile/create', 'add')->middleware('auth');
 });
 
 use App\Http\Controllers\Admin\ProfileController;
@@ -39,3 +30,4 @@ Route::controller(ProfileController::class)->prefix('admin')->group(function() {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
